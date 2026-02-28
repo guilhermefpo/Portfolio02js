de37fe2203d237a686d999f001105ed3cff08d46
Guilherme <gfernandoportelaoliveira@gmail.com>
2026-02-28 16:25:04 -0300

    Adicionando saldação com JavaScript

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,19 @@ toggle.addEventListener("click", () => {
     icon.classList.replace("fa-sun", "fa-moon");
   }
 });
+
+function carregar() {
+  const msg = document.getElementById("msg");
+  const hora = new Date().getHours();
+  let saudacao = "";
+
+  if (hora <= 12) {
+    saudacao = "Bom Dia!";
+  } else if (hora <= 18) {
+    saudacao = "Boa Tarde!";
+  } else {
+    saudacao = "Boa Noite!";
+  }
+
+  msg.innerHTML = `<h1>${saudacao}</h1>`;
+}
